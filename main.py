1cf3f27ac28682f5fb61d8bcde37aee903e2092e
Nik Tysiachnyi <nikita.tysiachnyi@gmail.com>
2025-08-10 15:36:15 +0200

    Completed script

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from ast import List
 import os
 import requests
 import json
@@ -6,7 +5,14 @@ from pathlib import Path
 from dotenv import load_dotenv
 from typing import List
 
-load_dotenv(dotenv_path=Path(__file__).resolve().parent / ".env.local", override=False)
+IS_GITHUB = os.getenv("GITHUB_ACTIONS") == True  # or: os.getenv("CI") == "true"
+
+if not IS_GITHUB:
+    load_dotenv(
+        dotenv_path=Path(__file__).resolve().parent / ".env.local", override=False
+    )
+
+
 AUTH_TOKEN = os.getenv("AUTH_TOKEN")
 GIST_ID = os.getenv("GIST_ID")
 SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")
@@ -43,9 +49,39 @@ def rotate(developers):
     return developers[1:] + developers[:1]
 
 
+def update_gist(data: List[str]) -> None:
+    url = f"https://api.github.com/gists/{GIST_ID}"
+    headers = {
+        "Authorization": f"Bearer {AUTH_TOKEN}",
+        "Accept": "application/vnd.github+json",
+    }
+    payload = {
+        "files": {
+            GIST_FILE_NAME: {
+                "content": json.dumps(data, indent=2),
+            }
+        }
+    }
+    r = requests.patch(url, headers=headers, json=payload, timeout=15)
+    r.raise_for_status()
+
+
+def slack_message(developers):
+    message = "👨‍💻 Weekly Dev Schedule\n\n" + "\n".join(f"- {d}" for d in developers)
+    print(message)
+    r = requests.post(
+        SLACK_WEBHOOK_URL,
+        json={"text": message},
+        timeout=15,
+    )
+    r.raise_for_status()
+
+
 def main():
     developers = get_developers()
     new_developers = rotate(developers)
+    update_gist(new_developers)
+    slack_message(new_developers)
 
 
 if __name__ == "__main__":
